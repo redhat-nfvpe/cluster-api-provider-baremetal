6f1f710ea793957a12a5322aa35eda2df928e7a4
Ricardo Noriega <rnoriega@redhat.com>
2019-01-25 14:59:47 +0100

    Adding API machinery to write BaremetalMachine configs to etcd

--- pkg/apis/baremetalmachineproviderconfig/v1alpha1/baremetalmachineproviderconfig_types.go
+++ pkg/apis/baremetalmachineproviderconfig/v1alpha1/baremetalmachineproviderconfig_types.go
@@ -16,3 +16,25 @@ type Ignition struct {
 	// Ignition config to be run during bootstrapping
 	UserDataSecret string `json:"userDataSecret"`
 }
+
+type BaremetalMachineProviderStatus struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// BaremetalMachineID is a unique identifier for the baremetal machine
+	BaremetalMachineID *string `json:"baremetalMachineID"`
+
+	// BaremetalMachineState is the current state of the baremetal machine
+	// TODO: define a state machine for baremetal
+	BaremetalMachineState *string `json:"baremetalMachineState"`
+}
+
+// BaremetalMachineProviderConfigList contains a list of BaremetalMachineProviderConfig
+type BaremetalMachineProviderConfigList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []BaremetalMachineProviderConfig `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&BaremetalMachineProviderConfig{}, &BaremetalMachineProviderConfigList{}, &BaremetalMachineProviderStatus{})
+}
---
+++ pkg/apis/baremetalmachineproviderconfig/v1alpha1/doc.go
@@ -0,0 +1,7 @@
+// Package v1alpha1 contains API Schema definitions for the baremetalproviderconfig v1alpha1 API group
+// +k8s:openapi-gen=true
+// +k8s:deepcopy-gen=package,register
+// +k8s:conversion-gen=github.com/openshift/cluster-api-provider-baremetal/pkg/apis/baremetalproviderconfig
+// +k8s:defaulter-gen=TypeMeta
+// +groupName=baremetalproviderconfig.k8s.io
+package v1alpha1

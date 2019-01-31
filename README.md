33cddf465048e5acd7f136507a783a027e1c7571
Ricardo Noriega <rnoriega@redhat.com>
2019-01-31 13:36:12 +0100

    WIP: Adding machine actuator

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 When using the k8s code-generator against this, make sure you have installed k8s.io/apimachinery!
 
+go get -u k8s.io/code-generator
 go get -u k8s.io/apimachinery
 
 https://github.com/kubernetes/code-generator/issues/21
--- pkg/apis/baremetalproviderconfig/v1alpha1/register.go
+++ pkg/apis/baremetalproviderconfig/v1alpha1/register.go
@@ -1,9 +1,26 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package v1alpha1
 
 import (
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/runtime/scheme"
 
 	"github.com/redhat-nfvpe/cluster-api-provider-baremetal/pkg/apis/baremetalproviderconfig"
 )
@@ -11,6 +28,9 @@ import (
 var (
 	// SchemeGroupVersion is group version used to register these objects
 	SchemeGroupVersion = schema.GroupVersion{Group: baremetalproviderconfig.GroupName, Version: "v1alpha1"}
+
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
+	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
 )
 
 // Kind takes an unqualified kind and returns back a Group qualified GroupKind
@@ -23,13 +43,7 @@ func Resource(resource string) schema.GroupResource {
 	return SchemeGroupVersion.WithResource(resource).GroupResource()
 }
 
-var (
-	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
-	AddToScheme   = SchemeBuilder.AddToScheme
-)
-
 // addKnownTypes adds our types to the API scheme by registering
-// MyResource and MyResourceList
 func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(
 		SchemeGroupVersion,

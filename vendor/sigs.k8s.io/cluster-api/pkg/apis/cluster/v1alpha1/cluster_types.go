f9ed153e171c45bb09d1dee5056f2c27ea30f920
Andrew Bays <andrew.bays@gmai.com>
2019-02-11 14:51:31 -0500

    Trying to fix deps, breaks test command

--- vendor/sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha1/cluster_types.go
+++ vendor/sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha1/cluster_types.go
@@ -19,6 +19,7 @@ package v1alpha1
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	"sigs.k8s.io/cluster-api/pkg/apis/cluster/common"
 )
@@ -65,7 +66,7 @@ type ClusterNetworkingConfig struct {
 	// The network ranges from which service VIPs are allocated.
 	Services NetworkRanges `json:"services"`
 
-	// The network ranges from which Pod networks are allocated.
+	// The network ranges from which POD networks are allocated.
 	Pods NetworkRanges `json:"pods"`
 
 	// Domain name for services.
